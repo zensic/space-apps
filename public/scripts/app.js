c3f0b7159eb3ef59e7cb9010aea948e174849af3
zensic <anthonyleehj@gmail.com>
2021-10-03 02:46:30 +0800

    Updated scenario in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
   console.log("Firebase initialized!");
 });
 
+var soundCheck = true;
+function configureAudio(audioID) {
+  const audio = document.getElementById(audioID);
+  if (soundCheck == true) {
+    soundCheck = false;
+    audio.pause();
+  } else if (soundCheck == false) {
+    soundCheck = true;
+    audio.play();
+  }
+}
+
+function adjustVolume(audioID) {
+  const audio = document.getElementById(audioID);
+  audio.volume = 0.2; //Set a fix volume value
+}
+
 function setTest() {
   console.log("setTest fired!");
   // Add a new document in collection "cities"
@@ -19,30 +36,6 @@ function setTest() {
     })
     .catch((error) => {
       console.error("Error writing document: ", error);
-    });
-}
-
-function getTest() {
-  console.log("getTest fired!");
-
-  const db = firebase.firestore();
-  var docRef = db
-    .collection("scenario")
-    .doc("1")
-    .collection("action-1")
-    .doc("desc");
-  docRef
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        console.log("Document data:", doc.data());
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    })
-    .catch((error) => {
-      console.log("Error getting document:", error);
     });
 }
 
@@ -106,15 +99,87 @@ function handleCommand(command) {
   history.scrollTop = history.scrollHeight;
 }
 
-command_input.addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
-    e.preventDefault();
-    handleCommand(command_input.value);
-    command_input.value = "";
-  }
-});
+function initEnterListener($month, $stats) {
+  command_input.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      handleCommand(command_input.value);
+      getEffect($month, command_input.value, $stats);
 
-function readInput(command) {}
+      var fish_fillet = `
+      You are free to explore! Here's what you can do:<br/>
+      1. Check Health<br/>
+      2. Check Radiation Levels<br/>
+      3. Eat fruits and vegetables<br/>
+      4. Exercise<br/>
+      5. Take radiation medicine<br/><br/>
+      `;
+      handleCommand(fish_fillet);
+      getFillerTask(command_input.value, $stats);
+
+      command_input.value = "";
+    }
+  });
+}
+
+function getFillerTask($index, $stats) {
+  const db = firebase.firestore();
+  var docRef = db.collection("filler_task").doc($index);
+
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+
+        console.log(doc.data().name);
+
+        var response = "";
+
+        if (doc.data().type == "check") {
+          var threshold = doc.data().threshold;
+          var to_check = doc.data().to_check;
+          var ok = true;
+
+          switch (to_check) {
+            case "hp":
+              ok = $stats[to_check] >= threshold;
+              break;
+            case "mm":
+              ok = $stats[to_check] >= threshold;
+              break;
+            case "bm":
+              ok = $stats[to_check] >= threshold;
+              break;
+            case "rl":
+              ok = $stats[to_check] <= threshold;
+              break;
+            case "sl":
+              ok = $stats[to_check] <= threshold;
+              break;
+          }
+
+          response = ok
+            ? doc.data().positive_respond
+            : doc.data().negative_respond;
+        } else if (doc.data().type == "action") {
+          updateStatus(
+            doc.data().hp,
+            doc.data().mm,
+            doc.data().bm,
+            doc.data().rl,
+            doc.data().sl,
+            $stats
+          );
+          response = doc.data().positive_respond;
+        }
+
+        handleCommand(response);
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting Filler Task:", error);
+    });
+}
 
 // adds one to the count and updates the dom
 function addMonth($month) {
@@ -141,21 +206,91 @@ function getScenario($index) {
     });
 }
 
-// gets all actions of the given index
-function getActions($index) {
+// gets the effect of the action
+// accepts two strings
+function getStats($index, $action, $stats) {
+  var sAction = "action-1";
+
+  switch ($action) {
+    case "1":
+      var sAction = "action-1";
+      break;
+    case "2":
+      var sAction = "action-2";
+      break;
+    case "3":
+      var sAction = "action-3";
+      break;
+  }
+
   const db = firebase.firestore();
   var docRef = db
     .collection("scenario")
-    .doc($index)
-    .collection("action-1")
+    .doc($index[0].toString())
+    .collection(sAction)
     .doc("stats");
   docRef
     .get()
     .then((doc) => {
       if (doc.exists) {
-        console.log(doc.data());
-        handleCommand(doc.data().hp);
+        var eHp = doc.data().hp ? doc.data().hp : 0;
+        var eMm = doc.data().mm ? doc.data().mm : 0;
+        var eBm = doc.data().bm ? doc.data().bm : 0;
+        var eRl = doc.data().rl ? doc.data().rl : 0;
+        var eSl = doc.data().sl ? doc.data().sl : 0;
+        
+        updateStatus(eHp, eMm, eBm, eRl, eSl, $stats);
+
+        // advance to next month
+        $index[0] = $index[0] + 1; 
+
+        // goes to the next scenario
+        getScenario($index[0].toString());
       } else {
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+}
+
+// gets the effect of the action
+// accepts two strings
+function getEffect($index, $action, $stats) {
+  var sAction = "action-1";
+
+  switch ($action) {
+    case "1":
+      var sAction = "action-1";
+      break;
+    case "2":
+      var sAction = "action-2";
+      break;
+    case "3":
+      var sAction = "action-3";
+      break;
+  }
+
+  const db = firebase.firestore();
+  var docRef = db
+    .collection("scenario")
+    .doc($index[0].toString())
+    .collection(sAction)
+    .doc("desc");
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        handleCommand(doc.data().effect); // print effect of action
+
+        // updates the whole stats
+        getStats($index, $action, $stats);
+
+        // goes to the next scenario
+        // getScenario($index[0].toString());
+      } else {
+        
         console.log("No such document!");
       }
     })
@@ -178,21 +313,34 @@ function validateInput($input, $numInputs) {
 }
 
 // updates the current player status
+// accepts number of stat to change
 function updateStatus($hp, $mm, $bm, $rl, $sl, $stsArr) {
-
+  console.log($hp);
+  $stsArr["hp"] = ($stsArr["hp"] + $hp < 0) ? 0 : ($stsArr["hp"] + $hp > 100) ? 100 : $stsArr["hp"] + $hp;
+  $stsArr["mm"] = ($stsArr["mm"] + $mm < 0) ? 0 : ($stsArr["mm"] + $mm > 100) ? 100 : $stsArr["mm"] + $mm;
+  $stsArr["bm"] = ($stsArr["bm"] + $bm < 0) ? 0 : ($stsArr["bm"] + $bm > 100) ? 100 : $stsArr["hp"] + $bm;
+  $stsArr["rl"] = ($stsArr["rl"] + $rl < 0) ? 0 : ($stsArr["rl"] + $rl > 100) ? 100 : $stsArr["rl"] + $rl;
+  $stsArr["sl"] = ($stsArr["sl"] + $sl < 0) ? 0 : ($stsArr["sl"] + $sl > 100) ? 100 : $stsArr["sl"] + $sl;
+  console.log($sl);
+  // updates the dom model
+  document.getElementById("hp").innerHTML = $stsArr["hp"];
+  document.getElementById("mm").innerHTML = $stsArr["mm"];
+  document.getElementById("bm").innerHTML = $stsArr["bm"];
+  document.getElementById("rl").innerHTML = $stsArr["rl"];
+  document.getElementById("sl").innerHTML = $stsArr["sl"];
 }
 
 // if status < 100 / status > 100, returns true if alive, false if dead
-function checkStatus($hp, $mm, $bm, $rl, $sl) {
-  if ($hp < 0) {
+function checkStatus($stats) {
+  if ($stats["hp"] < 0) {
     return false;
-  } else if ($mm < 0) {
+  } else if ($stats["mm"] < 0) {
     return false;
-  } else if ($bm < 0) {
+  } else if ($stats["bm"] < 0) {
     return false;
-  } else if ($rl > 100) {
+  } else if ($stats["rl"] > 100) {
     return false;
-  } else if ($sl > 100) {
+  } else if ($stats["sl"] > 100) {
     return false;
   }
   return true;
@@ -201,11 +349,11 @@ function checkStatus($hp, $mm, $bm, $rl, $sl) {
 // if no. of months > 3 years, player reaches mars, return true, else return false
 function checkMonths($months) {}
 
+// initialize first scenario
 function initScenario() {
   document.addEventListener("DOMContentLoaded", (event) => {
     getScenario("0");
     getScenario("1");
-    getActions("1");
   });
 }
 

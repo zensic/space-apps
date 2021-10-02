81aa1cc0b8bea30dac505e7e5a99e263c20fb73c
zensic <anthonyleehj@gmail.com>
2021-10-02 16:00:59 +0800

    Updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,7 +26,11 @@ function getTest() {
   console.log("getTest fired!");
 
   const db = firebase.firestore();
-  var docRef = db.collection("scenario").doc("1").collection("action-1").doc("desc");
+  var docRef = db
+    .collection("scenario")
+    .doc("1")
+    .collection("action-1")
+    .doc("desc");
   docRef
     .get()
     .then((doc) => {
@@ -42,12 +46,13 @@ function getTest() {
     });
 }
 
-function getAvatar($avatar) {
-  console.log("getTest fired!");
+/* avatar selection start */
+function setAvatar($avatar, $sessionAvatar) {
+  $sessionAvatar[0] = $avatar; // assign avatr to session avatar
 
   const db = firebase.firestore();
   var docRef = db.collection("avatar").doc($avatar);
-  
+
   docRef
     .get()
     .then((doc) => {
@@ -61,7 +66,12 @@ function getAvatar($avatar) {
           document
             .getElementById("avatar-img")
             .getElementsByTagName("img")[0].alt = $avatar;
-          document.getElementById("avatar-desc").innerHTML = doc.data().description;
+          document
+            .getElementById("avatar-desc")
+            .getElementsByTagName("h3")[0].innerHTML = $avatar;
+          document
+            .getElementById("avatar-desc")
+            .getElementsByTagName("p")[0].innerHTML = doc.data().description;
         } else {
           console.log("N0 avatar data found!");
         }
@@ -75,6 +85,11 @@ function getAvatar($avatar) {
       console.log("Error getting document:", error);
     });
 
-    return [];
+  return [];
 }
 
+function handleChoose($sessionAvatar) {
+  sessionStorage.setItem("avatar", $sessionAvatar);
+  window.location.href='gameplay.html';
+}
+/* avatar selection end */

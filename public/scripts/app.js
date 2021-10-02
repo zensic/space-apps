44b93bcd0249ab91ce4b362cf872d0c1c266de6a
zensic <anthonyleehj@gmail.com>
2021-10-02 23:25:29 +0800

    Updated functions for app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -73,7 +73,7 @@ function setAvatar($avatar, $sessionAvatar) {
             .getElementById("avatar-desc")
             .getElementsByTagName("p")[0].innerHTML = doc.data().description;
         } else {
-          console.log("N0 avatar data found!");
+          console.log("No avatar data found!");
         }
         //return [doc.data().image, doc.data().description];
       } else {
@@ -114,6 +114,8 @@ command_input.addEventListener("keydown", (e) => {
   }
 });
 
+function readInput(command) {}
+
 // adds one to the count and updates the dom
 function addMonth($month) {
   $month[0] = $month[0] + 1;
@@ -139,10 +141,108 @@ function getScenario($index) {
     });
 }
 
+// gets all actions of the given index
+function getActions($index) {
+  const db = firebase.firestore();
+  var docRef = db
+    .collection("scenario")
+    .doc($index)
+    .collection("action-1")
+    .doc("stats");
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log(doc.data());
+        handleCommand(doc.data().hp);
+      } else {
+        console.log("No such document!");
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+    });
+}
+
+// validates the input field, returns true if passed
+function validateInput($input, $numInputs) {
+  try {
+    if (parseInt($input) <= $numInputs && parseInt($input) > 0) {
+      return true;
+    }
+  } catch (err) {
+    console.log(err.message);
+    return false;
+  }
+  return false;
+}
+
+// updates the current player status
+function updateStatus($hp, $mm, $bm, $rl, $sl, $stsArr) {
+
+}
+
+// if status < 100 / status > 100, returns true if alive, false if dead
+function checkStatus($hp, $mm, $bm, $rl, $sl) {
+  if ($hp < 0) {
+    return false;
+  } else if ($mm < 0) {
+    return false;
+  } else if ($bm < 0) {
+    return false;
+  } else if ($rl > 100) {
+    return false;
+  } else if ($sl > 100) {
+    return false;
+  }
+  return true;
+}
+
+// if no. of months > 3 years, player reaches mars, return true, else return false
+function checkMonths($months) {}
+
 function initScenario() {
   document.addEventListener("DOMContentLoaded", (event) => {
     getScenario("0");
+    getScenario("1");
+    getActions("1");
   });
 }
 
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 /* gameplay section end */
